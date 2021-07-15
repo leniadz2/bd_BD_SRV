9e4fe0c5101322f5adde6de17e273191d7e6aa0f
Daniel <leniadz2@gmail.com>
2021-07-15 11:04:55 -0500

    0715

--- Tables/plano.locacionComercial.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE TABLE [plano].[locacionComercial] (
-  [CodigoLocacion] [varchar](50) NULL,
-  [DescripcionMall] [varchar](150) NULL
-)
-ON [PRIMARY]
-GO
--- Tables/tmp.operadoresSRV.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [tmp].[operadoresSRV] (
-  [RUCEMISOR] [varchar](11) NULL,
-  [CODIGOMALL] [varchar](2) NULL,
-  [FECHA] [varchar](10) NULL,
-  [CODIGOTIENDA] [varchar](4) NULL,
-  [CONTRATO_RE] [varchar](10) NULL
-)
-ON [PRIMARY]
-GO
